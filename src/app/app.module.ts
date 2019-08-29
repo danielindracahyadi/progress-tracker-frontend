b19c0b1e9a8e5d14f1bb4defdd949c211e741338
Antonius Filian <antoniusfilian@gmail.com>
2019-08-29 15:30:24 +0700

    update core-ui datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,39 @@ import { AppComponent } from './app.component';
 import { CustomTextViewComponent } from './core-ui/custom-text-view/custom-text-view.component';
 import { TextInputComponent } from './core-ui/text-input/text-input.component';
 import { CustomButtonComponent } from './core-ui/custom-button/custom-button.component';
+import { CustomDatepickerComponent } from './core-ui/custom-datepicker/custom-datepicker.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRippleModule, MatInputModule } from '@angular/material';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomTextViewComponent,
     TextInputComponent,
-    CustomButtonComponent
+    CustomButtonComponent,
+    CustomDatepickerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatRippleModule,
+    MatMomentDateModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core-ui/custom-datepicker/custom-datepicker.component.ts
+++ src/app/core-ui/custom-datepicker/custom-datepicker.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MomentDateAdapter } from '@angular/material-moment-adapter';
 import { DateAdapter, 
   MAT_DATE_FORMATS, 
   MAT_DATE_LOCALE} from '@angular/material/core';
 import { MatDatepicker } from '@angular/material/datepicker';
+import { MatRippleModule, MatInputModule, MatIconModule } from '@angular/material';
 
 import * as _moment from 'moment';
 import { default as _rollupMoment, Moment } from 'moment';
@@ -24,18 +25,41 @@ export const MY_FORMATS = {
 };
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
-  providers: [
-    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
-
-    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
-  ],
+  selector: 'core-ui-datepicker',
+  template: `
+    <mat-form-field color="#0871CE" class="classdatepicker">
+	    <input matInput [matDatepicker]="myDatePicker" [formControl]="date" color="#0871CE">
+      <mat-datepicker-toggle matSuffix [for]="myDatePicker" color="#0871CE">
+      </mat-datepicker-toggle>
+      <mat-datepicker #myDatePicker
+        startView="multi-year"
+        (yearSelected)="chosenYearHandler($event)"
+        (monthSelected)="chosenMonthHandler($event, myDatePicker)"
+        panelClass="example-month-picker" color="#0871CE">
+      </mat-datepicker>
+    </mat-form-field>
+  `,
+  styles: [`
+  .classdatepicker {
+    text-align: left;
+    font: bold 18px/32px Source Sans Pro;
+    letter-spacing: 0;
+    color: #0871CE;
+    opacity: 1;
+    }
+  `],
+  encapsulation: ViewEncapsulation.Native
 })
-export class AppComponent {
+
+
+export class CustomDatepickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
   date = new FormControl(moment());
-  month : string[]
 
   chosenYearHandler(normalizedYear: Moment) {
     const ctrlValue = this.date.value;
