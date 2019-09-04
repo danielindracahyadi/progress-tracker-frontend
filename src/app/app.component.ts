64316dd45bfd3b8ba4395dbcfde40acfc79f0153
karelkkurnia <yondaime8@yahoo.com>
2019-09-04 21:04:34 +0700

    pure check-box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,4 @@ import { Component, Input, Output } from '@angular/core';
 })
 export class AppComponent {
 
-  @Output() dummyCardReportTask = [
-    {
-      projectName : 'Dinas Kesehatan',
-      role : 'UI/UX Designer',
-      workTotal : '4'
-    },
-    {
-      projectName : 'MacroAd',
-      role : 'UI/UX Designer',
-      workTotal : '168'
-    }
-  ];
-
-  ngOnInit() {
-  }
 }
