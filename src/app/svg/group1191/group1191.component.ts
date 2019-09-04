0b54f69b42248e943bffdb352f9f113f49cebd43
karelkkurnia <yondaime8@yahoo.com>
2019-09-05 01:02:17 +0700

    [User[ Report Detail

---
+++ src/app/svg/group1191/group1191.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-group1191',
+  templateUrl: './group1191.component.html',
+  styleUrls: ['./group1191.component.sass']
+})
+export class Group1191Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
