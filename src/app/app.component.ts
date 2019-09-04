4adb30967e7a9396cda525caf213fab3cb442585
karelkkurnia <yondaime8@yahoo.com>
2019-09-04 14:45:27 +0700

    update Check-Box dan [User] Report Detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component, Input, Output } from '@angular/core';
 })
 export class AppComponent {
 
+  @Output() dummyCardReportTask = [
+    {
+      projectName : 'Dinas Kesehatan',
+      role : 'UI/UX Designer',
+      workTotal : '4'
+    },
+    {
+      projectName : 'MacroAd',
+      role : 'UI/UX Designer',
+      workTotal : '168'
+    }
+  ];
+
+  ngOnInit() {
+  }
 }
