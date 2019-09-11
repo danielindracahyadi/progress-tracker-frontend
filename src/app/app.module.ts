be56785fdc3ce40b197413738fdadf4ba94fc191
sherlyflorencia <sherlyflorencia01@gmail.com>
2019-09-11 23:19:17 +0700

    'updateMainDashboard'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TextViewComponent } from './core-ui/text-view/text-view.component';
