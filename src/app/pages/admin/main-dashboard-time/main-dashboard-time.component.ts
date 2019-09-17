cffad48c44758cdf468ddd45c814d5c72c5ebbec
sherlyflorencia <sherlyflorencia01@gmail.com>
2019-09-17 13:57:56 +0700

    Main Dashboard Timeline 3

--- src/app/pages/admin/main-dashboard-time/main-dashboard-time.component.ts
+++ src/app/pages/admin/main-dashboard-time/main-dashboard-time.component.ts
@@ -1,212 +1,208 @@
 import { Component, OnInit,Input,Output } from '@angular/core';
-
+import { TIMELINE } from '../../../constants/index';
 
 @Component({
-  selector: 'app-main-dashboard-time',
+  selector: 'page-admin-main-dashboard-time',
   templateUrl: './main-dashboard-time.component.html',
   styleUrls: ['./main-dashboard-time.component.sass']
 })
 export class MainDashboardTimeComponent implements OnInit {
 
-  @Input() dataDummy: any;
-  selectedData: any;
+ timelines = TIMELINE;
 
-  @Output() dummyMainDashboardTime =[
-    {
-         date: 'Rabu, 19 Desember 2019',
-         otherProjects: 2,
-         projects: [
-            {
-               project: 'Back-end Engineer'
-            },
-            {
-               project: 'Front-end Engineer'
-            },
-            {
-               project: 'Project Manager'
-            }
-         ]
-      },
-      {
-         date: 'Selasa, 20 Desember 2019',
-         otherProjects: 2,
-         projects: [
-            {
-               project: 'Back-end Engineer'
-            },
-            {
-               project: 'Front-end Engineer'
-            },
-            {
-               project: 'Project Manager'
-            }
-         ]
-   },
-   {
-      date: 'Selasa, 21 Desember 2019',
-      otherProjects: 2,
-      projects: [
-         {
-            project: 'Back-end Engineer'
-         },
-         {
-            project: 'Front-end Engineer'
-         },
-         {
-            project: 'Project Manager'
-         }
-      ]
-   },
-   {
-      date: 'Selasa, 22 Desember 2019',
-      otherProjects: 2,
-      projects: [
-         {
-            project: 'Back-end Engineer'
-         },
-         {
-            project: 'Front-end Engineer'
-         },
-         {
-            project: 'Project Manager'
-         }
-      ]
-   },
-   {
-      date: 'Selasa, 20 Desember 2019',
-      otherProjects: 2,
-      projects: [
-         {
-            project: 'Back-end Engineer'
-         },
-         {
-            project: 'Front-end Engineer'
-         },
-         {
-            project: 'Project Manager'
-         }
-      ]
-   },
-   {
-      date: 'Selasa, 20 Desember 2019',
-      otherProjects: 2,
-      projects: [
-         {
-            project: 'Back-end Engineer'
-         },
-         {
-            project: 'Front-end Engineer'
-         },
-         {
-            project: 'Project Manager'
-         }
-      ]
-   },
-   {
-      date: 'Selasa, 21 Desember 2019',
-      otherProjects: 2,
-      projects: [
-         {
-            project: 'Back-end Engineer'
-         },
-         {
-            project: 'Front-end Engineer'
-         },
-         {
-            project: 'Project Manager'
-         }
-      ]
-   },
-   {
-      date: 'Selasa, 22 Desember 2019',
-      otherProjects: 2,
-      projects: [
-         {
-            project: 'Back-end Engineer'
-         },
-         {
-            project: 'Front-end Engineer'
-         },
-         {
-            project: 'Project Manager'
-         }
-      ]
-   },
-   {
-      date: 'Selasa, 20 Desember 2019',
-      otherProjects: 2,
-      projects: [
-         {
-            project: 'Back-end Engineer'
-         },
-         {
-            project: 'Front-end Engineer'
-         },
-         {
-            project: 'Project Manager'
-         }
-      ]
-   },
-   {
-      date: 'Selasa, 20 Desember 2019',
-      otherProjects: 2,
-      projects: [
-         {
-            project: 'Back-end Engineer'
-         },
-         {
-            project: 'Front-end Engineer'
-         },
-         {
-            project: 'Project Manager'
-         }
-      ]
-   },
-   {
-      date: 'Selasa, 21 Desember 2019',
-      otherProjects: 2,
-      projects: [
-         {
-            project: 'Back-end Engineer'
-         },
-         {
-            project: 'Front-end Engineer'
-         },
-         {
-            project: 'Project Manager'
-         }
-      ]
-   },
-   {
-      date: 'Selasa, 22 Desember 2019',
-      otherProjects: 2,
-      projects: [
-         {
-            project: 'Back-end Engineer'
-         },
-         {
-            project: 'Front-end Engineer'
-         },
-         {
-            project: 'Project Manager'
-         }
-      ]
-   }
- ];
+//   @Output() dummyMainDashboardTime =[
+//     {
+//          date: '2019-12-19',
+//          otherProjects: 2,
+//          projects: [
+//             {
+//                project: 'Back-end Engineer'
+//             },
+//             {
+//                project: 'Front-end Engineer'
+//             },
+//             {
+//                project: 'Project Manager'
+//             }
+//          ]
+//       },
+//       {
+//          date: '2019-12-20',
+//          otherProjects: 2,
+//          projects: [
+//             {
+//                project: 'Back-end Engineer'
+//             },
+//             {
+//                project: 'Front-end Engineer'
+//             },
+//             {
+//                project: 'Project Manager'
+//             }
+//          ]
+//    },
+//    {
+//       date: '2019-12-21',
+//       otherProjects: 2,
+//       projects: [
+//          {
+//             project: 'Back-end Engineer'
+//          },
+//          {
+//             project: 'Front-end Engineer'
+//          },
+//          {
+//             project: 'Project Manager'
+//          }
+//       ]
+//    },
+//    {
+//       date: '2019-12-22',
+//       otherProjects: 2,
+//       projects: [
+//          {
+//             project: 'Back-end Engineer'
+//          },
+//          {
+//             project: 'Front-end Engineer'
+//          },
+//          {
+//             project: 'Project Manager'
+//          }
+//       ]
+//    },
+//    {
+//       date: '2019-12-20',
+//       otherProjects: 2,
+//       projects: [
+//          {
+//             project: 'Back-end Engineer'
+//          },
+//          {
+//             project: 'Front-end Engineer'
+//          },
+//          {
+//             project: 'Project Manager'
+//          }
+//       ]
+//    },
+//    {
+//       date: '2019-12-21',
+//       otherProjects: 2,
+//       projects: [
+//          {
+//             project: 'Back-end Engineer'
+//          },
+//          {
+//             project: 'Front-end Engineer'
+//          },
+//          {
+//             project: 'Project Manager'
+//          }
+//       ]
+//    },
+//    {
+//       date: '2019-12-21',
+//       otherProjects: 2,
+//       projects: [
+//          {
+//             project: 'Back-end Engineer'
+//          },
+//          {
+//             project: 'Front-end Engineer'
+//          },
+//          {
+//             project: 'Project Manager'
+//          }
+//       ]
+//    },
+//    {
+//       date: '2019-12-22',
+//       otherProjects: 2,
+//       projects: [
+//          {
+//             project: 'Back-end Engineer'
+//          },
+//          {
+//             project: 'Front-end Engineer'
+//          },
+//          {
+//             project: 'Project Manager'
+//          }
+//       ]
+//    },
+//    {
+//       date: '2019-12-20',
+//       otherProjects: 2,
+//       projects: [
+//          {
+//             project: 'Back-end Engineer'
+//          },
+//          {
+//             project: 'Front-end Engineer'
+//          },
+//          {
+//             project: 'Project Manager'
+//          }
+//       ]
+//    },
+//    {
+//       date: '2019-12-20',
+//       otherProjects: 2,
+//       projects: [
+//          {
+//             project: 'Back-end Engineer'
+//          },
+//          {
+//             project: 'Front-end Engineer'
+//          },
+//          {
+//             project: 'Project Manager'
+//          }
+//       ]
+//    },
+//    {
+//       date: '2019-12-21',
+//       otherProjects: 2,
+//       projects: [
+//          {
+//             project: 'Back-end Engineer'
+//          },
+//          {
+//             project: 'Front-end Engineer'
+//          },
+//          {
+//             project: 'Project Manager'
+//          }
+//       ]
+//    },
+//    {
+//       date: '2019-12-22',
+//       otherProjects: 2,
+//       projects: [
+//          {
+//             project: 'Back-end Engineer'
+//          },
+//          {
+//             project: 'Front-end Engineer'
+//          },
+//          {
+//             project: 'Project Manager'
+//          }
+//       ]
+//    }
+//  ];
 
  onClick(){
     console.log("Card id Clicked!");
  }
 
   constructor() { }
-
-  consoleTimeline(i){
-   console.log(this.dummyMainDashboardTime[i]);
-  }
   ngOnInit() {
-  }
-  change(data) {
-    this.selectedData = [data];
-  }
+   }
+
+//   change(data) {
+//     this.selectedData = [data];
+//   }
 
 }
