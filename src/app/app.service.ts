255699a291fe0b61a9515c58a9c92a3a8220c065
sherlyflorencia <sherlyflorencia01@gmail.com>
2019-09-30 23:02:23 +0700

    13

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,4 @@
 import { Injectable, EventEmitter, Output } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
 
 @Injectable({
   providedIn: 'root'
