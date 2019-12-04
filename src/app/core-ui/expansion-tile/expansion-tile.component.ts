85507e05eec8e10bbb516609d628049b5c696787
Karel Kurnia <yondaime8@yahoo.com>
2019-12-04 11:31:44 +0700

    clear bug

--- src/app/core-ui/expansion-tile/expansion-tile.component.ts
+++ src/app/core-ui/expansion-tile/expansion-tile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { AppService } from './../../app.service';
 import { MainDashboardService } from 'src/app/pages/admin/main-dashboard/main-dashboard.service';
 import { MainDashboardComponent } from 'src/app/pages/admin/main-dashboard/main-dashboard.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'core-ui-expansion-tile',
@@ -13,15 +14,18 @@ export class ExpansionTileComponent implements OnInit {
   showProject = true;
   showReport = false;
   showUser = false;
+  featureTambah = '';
 
   @Input() text = '';
   constructor(
     private appService: AppService,
     private mainDashboardService: MainDashboardService,
+    private router: Router,
     private mainDashboard: MainDashboardComponent,
   ) {}
 
   ngOnInit() {
+    this.featureTambah = this.mainDashboard.featureTambah;
     if (this.mainDashboard.selectedMainDashboardDropdown === 'User') {
       this.showLihatDetailRole = false;
       this.showProject = false;
@@ -41,7 +45,15 @@ export class ExpansionTileComponent implements OnInit {
   }
 
   doDetail() {
-    console.log('Lihat Detail ' + this.mainDashboard.theCurrentChosen);
+    this.mainDashboardService.selectedID = this.mainDashboard.theCurrentChosenID;
+    this.mainDashboardService.selectedName = this.mainDashboard.theCurrentChosen;
+    if(this.mainDashboard.selectedMainDashboardDropdown === '' || this.mainDashboard.selectedMainDashboardDropdown === 'Project') {
+      this.mainDashboardService.selectedMainDashboardCategory = 'projects';
+      this.router.navigateByUrl('home-admin/main-dashboard-time');
+    } else if (this.mainDashboard.selectedMainDashboardDropdown === 'Role') {
+      this.mainDashboardService.selectedMainDashboardCategory = 'roles';
+      this.router.navigateByUrl('home-admin/main-dashboard-time');
+    }
   }
   doEdit() {
     console.log('Edit ' + this.mainDashboard.theCurrentChosen);
